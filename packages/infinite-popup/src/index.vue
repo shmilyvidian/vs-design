a1af923b497a7bbd5f297e95459f96d229fe9b4c
frosv <616342684@qq.com>
2020-11-10 17:37:54 +0800

    feat: popup 加入接口更新

--- packages/infinite-popup/src/index.vue
+++ packages/infinite-popup/src/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="infinite-popup">
-    <Popup v-model="isShow"
-    @click-overlay="onClickOverlay">内容</Popup>
+    <Popup
+      v-model="isShow"
+      :zIndex="zIndex"
+      :overlay="overlay"
+      :position="position"
+      :duration="duration"
+      :round="round"
+      :closeOnClickOverlay="closeOnClickOverlay"
+      @click-overlay="onClickOverlay"
+    >内容</Popup>
   </div>
 </template>
 
@@ -24,7 +32,43 @@ export default {
       default: false,
       required: false
     },
-    
+    // z-index 层级
+    zIndex: {
+      type: Number,
+      default: 100,
+      required: false
+    },
+    // 是否显示遮罩层
+    overlay: {
+      type: Boolean,
+      default: true,
+      required: false
+    },
+    // 弹出位置，可选值为 top bottom right left
+    position: {
+      type: String,
+      default: 'center',
+      required: false
+    },
+    // 动画时长，单位为毫秒
+    duration: {
+      type: [number, object],
+      default: 300,
+      required: false
+    },
+    // 是否显示圆角
+    round: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+    // 是否在点击遮罩层后关闭
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+      required: false
+    }
+
   },
   data () {
     return {
@@ -41,10 +85,10 @@ export default {
     }
   },
   methods: {
-    onClickOverlay(){
-      console.log(1231232)
+    // 点击遮罩层时触发
+    onClickOverlay () {
       this.$emit('onClickOverlay')
     }
-  } 
+  }
 }
 </script>
