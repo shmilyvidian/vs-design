1453dad77a2e6c2d39c005d4d86061f0759dd28d
zhoubo <1522306890@qq.com>
2020-11-18 10:45:10 +0800

    feat(infinite-header-nav/index.vue): 修改了导航栏组件
    1,添加了是否展示返回ICON的属性.2,修改了title的显示样式,根据是否有右侧内容,调整居中样式.

--- packages/infinite-header-nav/src/index.vue
+++ packages/infinite-header-nav/src/index.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="header-wrapper" :style="insertStyle">
-    <div class="header-left" @click="goBack">
+    <div class="header-left" @click="goBack" :class="{'no-visibility': !showBackIcon}">
       <van-icon v-if="useVant" name="arrow-left" :color="color" :size="size">
       </van-icon>
       <img v-else class="arrow-icon" :style="{width: size}" src="./../imgs/arrow.png" alt="">
       <slot name="leftItem"/>
     </div>
-    <div class="header-title">
+    <div class="header-title" :class="{'right-padding': !hasRightSlot}">
       <slot name="centerItem"/>
     </div>
     <div class="header-right">
@@ -23,6 +23,10 @@ export default {
     VanIcon
   },
   props: {
+    showBackIcon: {
+      type: Boolean,
+      default: true
+    },
     // 是否使用vant icon，默认true
     useVant: {
       type: Boolean,
@@ -47,15 +51,28 @@ export default {
       }
     }
   },
+  data () {
+    return {
+      hasRightSlot: false
+    }
+  },
+  created () {
+    // 获取slots
+    const rightItem = this.$slots.rightItem
+    // 如果右侧slot 有内容
+    if (rightItem) {
+      this.hasRightSlot = true
+    };
+  },
   methods: {
-    goBack() {
-      console.log('child goBack');
-      this.$emit('goBack');
+    goBack () {
+      console.log('child goBack')
+      this.showBackIcon && this.$emit('goBack')
     }
   },
   mounted () {
-    console.log('insertStyle');
-    console.log(this.insertStyle);
+    console.log('insertStyle')
+    console.log(this.insertStyle)
   }
 }
 </script>
