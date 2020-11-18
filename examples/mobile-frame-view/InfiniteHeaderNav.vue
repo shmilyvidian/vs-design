c481ce446e42b6d9076df6706e4544df2d25f0dd
zhoubo <1522306890@qq.com>
2020-11-18 10:55:33 +0800

    feat(infiniteheadernav.vue): 格式化了使用说明,增加了样例

--- examples/mobile-frame-view/InfiniteHeaderNav.vue
+++ examples/mobile-frame-view/InfiniteHeaderNav.vue
@@ -5,18 +5,28 @@
       <h2 class="infinite-doc-demo-block-title">基础用法</h2>
       <infinite-header-nav :useVant="useVant"></infinite-header-nav>
     </el-row>
+
+    <el-row class="infinite-doc-demo-block-row">
+      <h2 class="infinite-doc-demo-block-title">showBackIcon 显示左侧ICON</h2>
+      <infinite-header-nav :showBackIcon="showBackIcon" color="#fff">
+        <div slot="centerItem">
+          标题
+        </div>
+      </infinite-header-nav>
+    </el-row>
+
     <el-row class="infinite-doc-demo-block-row">
       <h2 class="infinite-doc-demo-block-title">size自定义icon大小</h2>
-
       <infinite-header-nav size="30px"></infinite-header-nav>
     </el-row>
+
     <el-row class="infinite-doc-demo-block-row">
       <h2 class="infinite-doc-demo-block-title">color自定义icon颜色</h2>
       <infinite-header-nav color="#fff"></infinite-header-nav>
     </el-row>
+
     <el-row class="infinite-doc-demo-block-row">
       <h2 class="infinite-doc-demo-block-title">slot自定义内容</h2>
-
       <infinite-header-nav>
         <div slot="leftItem">
           返回
@@ -29,10 +39,10 @@
         </div>
       </infinite-header-nav>
     </el-row>
+
     <el-row class="infinite-doc-demo-block-row">
       <h2 class="infinite-doc-demo-block-title">insertStyle自定义样式</h2>
-
-      <infinite-header-nav 
+      <infinite-header-nav
         :insertStyle="insertStyle">
         <div slot="leftItem">
           左文字
@@ -45,19 +55,21 @@
         </div>
       </infinite-header-nav>
     </el-row>
+
   </div>
 </template>
 <script>
 export default {
   methods: {},
-  data() {
+  data () {
     return {
       useVant: false,
       headerTitle: '标题',
-      insertStyle: { background: '#fff', color: '#35b6e0', fontSize: '18px', paddingTop: '6px'}
-    };
+      showBackIcon: false,
+      insertStyle: { background: '#fff', color: '#35b6e0', fontSize: '18px', paddingTop: '6px' }
+    }
   }
-};
+}
 </script>
 
 <style></style>
