227a7d51a884e0bcfb0616f4c7a120021ca2067d
fish <925249766@qq.com>
2020-11-22 15:53:26 +0800

    增加header、empty组件的测试案例

--- tests/unit/InfiniteHeaderNav.spec.js
+++ tests/unit/InfiniteHeaderNav.spec.js
@@ -1,8 +1,10 @@
 import { mount } from '@vue/test-utils'
 import InfiniteHeaderNav from '@/packages/infinite-header-nav/src/index.vue'
+
 const wrapper = mount(InfiniteHeaderNav)
 
 describe('Infinite header nav.vue', () => {
+
   test('render InfiniteHeaderNav', () => {
     const wrapper = mount(InfiniteHeaderNav)
     expect(wrapper).toMatchSnapshot()
@@ -23,6 +25,16 @@ describe('Infinite header nav.vue', () => {
     wrapper.destroy()
   })
 
+  it('has size and color style', () => {
+    const wrapper = mount(InfiniteHeaderNav, {
+      propsData: {
+        size: '10px',
+      }
+    })
+    expect(wrapper.props().size).toContain('10px')
+    wrapper.destroy()
+  })
+
   it('should not render backIcon when showBackIcon is false', () => {
     const wrapper = mount(InfiniteHeaderNav, {
       propsData: {
@@ -35,7 +47,19 @@ describe('Infinite header nav.vue', () => {
     wrapper.destroy()
   })
   
-
+  it('test header slots', () => {
+    const wrapper = mount(InfiniteHeaderNav, {
+      slots: {
+        leftItem: 'leftItem',
+        centerItem: 'centerItem',
+        rightItem: 'rightItem'
+      }
+    })
+    expect(wrapper.html()).toMatch('leftItem')
+    expect(wrapper.html()).toMatch('centerItem')
+    expect(wrapper.html()).toMatch('rightItem')
+    wrapper.destroy()
+  })
 
 
   it('test click event', async () => {
