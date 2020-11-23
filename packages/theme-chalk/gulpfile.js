e80ba537e2581d7f8f51bf3d49a9a4fa89201511
shmilyvidian <shmilyvidian@163.com>
2020-11-23 16:36:43 +0800

    feat: glup增加vant中css文件处理

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -4,13 +4,24 @@ const { series, src, dest } = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
+const path = require('path')
+var minifyCSS = require('gulp-clean-css')
 
+const extralPath = path.resolve(process.cwd(), '../../node_modules/vant')
+console.log(extralPath + '/**/*.css', 'path')
 function compile () {
-  return src('./src/*.scss')
-    .pipe(sass.sync())
+  return src('./src/*.scss', extralPath + '/**/*.css')
+ 
+    .pipe(sass.sync({
+      includePaths: extralPath + '/**/*.css'
+    }))
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
+    }))
+    .pipe(minifyCSS({
+      relativeTo: extralPath,
+      processImport: true
     }))
     .pipe(cssmin())
     .pipe(dest('./lib'))
